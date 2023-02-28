fbbb526055beeaf49ae05d6cc9e8e73115ccd3f7
MAMAOLWETHU <thulisiweb04@gmail.com>
2023-02-28 09:09:19 +0200

    Add project files.

---
+++ object orientated inheritance/Inheritance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_orientated_inheritance;
+class Vehicle
+{
+    public string brand = "Ford";
+    public void honk()
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
+}
---
+++ object orientated inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_orientated_inheritance;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle myCar = new Vehicle();
+
+        Console.WriteLine(myCar.brand+ " " + myCar.brand);
+    }
+}
